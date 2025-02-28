227e0ceb8e9e93cb1a4b2814fd5eb6e5b2f55127
Alan Antony <voa.alan@gmail.com>
2025-02-28 19:13:38 +0530

    initialapp

---
+++ cyboruins_sosapp/android/app/src/main/kotlin/com/example/cyboruins_sosapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cyboruins_sosapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ cyboruins_sosapp/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final List<String> _emergencyContacts = [];
+
+  Future<void> _sendSOS() async {
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+    final message = 'SOS! Need help at: '
+        'https://maps.google.com/?q=${position.latitude},${position.longitude}';
+  }
+
+  void _addContact() {
+    setState(() {
+      _emergencyContacts.add('123-456-7890');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              if (value == 'add_contact') _addContact();
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                value: 'add_contact',
+                child: Text('Add Contact'),
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text('Emergency Contacts: $_emergencyContacts'),
+            ElevatedButton(
+              onPressed: _sendSOS,
+              child: const Text('Send SOS'),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _counter++;
+          });
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
