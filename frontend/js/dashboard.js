c5104562f0a2169c827f59018afaeff7c9c0d798
Codesandmore <chackochanme@gmail.com>
2025-03-01 11:03:38 +0530

    frontend base

---
+++ frontend/js/dashboard.js
@@ -0,0 +1,16 @@
+<button onclick="logout()">Logout</button>
+
+function viewCases() {
+    alert("Fetching cases...");
+}
+function reviewEvidence() {
+    alert("Opening Evidence Vault...");
+    window.location.href = "index.html";
+}
+
+function submitCase() {
+    alert("Redirecting to case submission...");
+}
+function trackCase() {
+    alert("Fetching case status...");
+}
